f38794b6b233e5a49bb9b866342619ea38d9b712
sazzad <sazzad@SAZZAD_LAPTOP>
2026-05-18 02:23:03 +0700

    Edit the app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+
+      <h1 className="title">My React Profile</h1>
+
+      <img
+        src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+        alt="nature"
+        className="image"
+      />
+
+      <div className="card">
+        <h2>About Me</h2>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! My name is Md Sazzad Hossain Sohag.
+          I am learning React JS in Web Programming Practicum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          React helps developers create interactive user interfaces
+          efficiently and quickly.
+        </p>
+      </div>
+
+      <div className="card">
+        <h2>My Skills</h2>
+
+        <p>✔ HTML</p>
+        <p>✔ CSS</p>
+        <p>✔ JavaScript</p>
+        <p>✔ React JS</p>
+      </div>
+
     </div>
   );
 }
